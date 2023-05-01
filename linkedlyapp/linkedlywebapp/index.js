9090bc5aae44639683eddd522096e095b45a046b
reddoy <rohanomalley@gmail.com>
2023-05-01 14:53:39 -0700

    node

--- linkedlyapp/linkedlywebapp/index.js
+++ linkedlyapp/linkedlywebapp/index.js
@@ -1,42 +1,30 @@
-const node = require('node');
-const fetch = require('node-fetch');
-const express = require('express');
-
-const openaiApiKey = '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';
-const app = express();
-const port = 3000;
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-let name = req.body.name;
+const axios = require('axios');
 
 const prompt = "Once upon a time";
 
+const openaiApiKey = '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';
+
 const params = {
-"model": "text-davinci-002",
-"temperature": 0.7,
-"max_tokens": 60,
-"top_p": 1,
-"frequency_penalty": 0,
-"presence_penalty": 0
+  "model": "text-davinci-002",
+  "temperature": 0.7,
+  "max_tokens": 60,
+  "top_p": 1,
+  "frequency_penalty": 0,
+  "presence_penalty": 0
 };
 
-fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-method: 'POST',
-headers: {
+axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+  prompt,
+  ...params,
+}, {
+  headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${openaiApiKey}`,
-},
-body: JSON.stringify({
-    prompt,
-    ...params,
-}),
+    'Authorization': `${openaiApiKey}`,
+  },
 })
-.then(response => response.json())
-.then(data => console.log(data.choices[0].text))
-.catch(error => console.error(error));
-
+.then(response => {
+  console.log(response.data.choices[0].text);
+})
+.catch(error => {
+  console.error(error);
+});
