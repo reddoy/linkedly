30579f7276728722c3433f4ebc598a3399258957
Rohan <rohanomalley@gmail.com>
2023-05-01 18:06:58 -0700

    added gitignore

---
+++ linkedlyapp/linkedlychromeext/app.js
@@ -0,0 +1,89 @@
+window.onload = function(){
+
+    // create a funciton that listens for the genScoresBtn to be clicked then
+    // runs the addScores() function in content.js. I am building a chrome extension. I keep getting an error
+    // saying that Uncaught (in promise) Error: Could not establish connection. Receiving end does not exist.
+    // write code that avoids this error
+
+    function saveListToStorage(list) {
+        chrome.storage.local.set({ "personList": list }, function() {
+            console.log("Value stored");
+            document.getElementById('email').value = '';
+            document.getElementById('note').value = '';
+            let addListBtn = document.getElementById('subbutton');
+            addListBtn.value = "Added!";
+            setTimeout(function(){
+                addListBtn.value = "Add to List";
+            }, 1000);
+          });
+      }
+
+    async function getScores() {
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        console.log([tab]);
+        const response = await chrome.tabs.sendMessage(tab.id, {message: "addScores"});
+        // do something with response here, not outside the function
+        console.log(response);
+    }
+
+    const form = document.getElementById('personForm');
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var url = tabs[0].url;
+        document.getElementById('linkUrl').value = url;
+    });
+
+    // create a funciton that listens for when the personForm is submitted
+    // then runs the addPerson(linkedUrl, Name) function and passes in the values
+    // from the form
+    
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        const linkedUrl = document.getElementById('linkUrl').value;
+        let email = document.getElementById('email').value;
+        let note = document.getElementById('note').value;
+        chrome.storage.local.get(["personList"], function(result) {
+            console.log(result.personList);
+            if (result.personList) {
+                personArr = result.personList;
+            } else {
+                personArr = [];
+            }
+            personArr.push([email, linkedUrl, note]);
+            saveListToStorage(personArr);
+          });
+    });
+
+    // create a function that listens for when the clearListBtn button is clicked
+    // then clears the personList from chrome storage
+    document.getElementById('clearListBtn').addEventListener('click', function(){
+        chrome.storage.local.clear(function() {
+            console.log("personList cleared");
+            // write code that changes the text of clearListBtn to say "List Cleared"
+            // for 1 second then changes back to "Clear List"
+            let clearListBtn = document.getElementById('clearListBtn');
+            clearListBtn.innerHTML = "List Cleared";
+            setTimeout(function(){
+                clearListBtn.innerHTML = "Clear List";
+            }, 1000);
+        });
+    });
+        
+    // create a function that listens for when the downloadPpl button is clicked
+    // then conversts each array in the personArr to a csv file and downloads it
+    // to the users computer
+    document.getElementById('downloadPpl').addEventListener('click', function(){
+        chrome.storage.local.get(["personList"], function(result) {
+            let csvContent = "data:text/csv;charset=utf-8,";
+            csvContent += "Email,LinkedIn URL,Note\r\n";
+            console.log(result.personList);
+            result.personList.forEach(function(rowArray){
+                let row = rowArray.join(",");
+                csvContent += row + "\r\n";
+            });
+            var encodedUri = encodeURI(csvContent);
+            window.open(encodedUri);
+          });
+
+    });
+}
---
+++ linkedlyapp/llServer/index.js
@@ -0,0 +1,20 @@
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(){
+    const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: "This is a test",
+    });
+    console.log(completion.data.choices[0].text);
+}
+
+runCompletion();
