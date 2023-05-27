34b8d67746b7df605d706ca4d7194fa7538268dc
reddoy <rohanomalley@gmail.com>
2023-05-27 13:08:08 -0700

    yup

--- linkedlyapp/linkedlychromeext/background.js
+++ linkedlyapp/linkedlychromeext/background.js
@@ -1,1 +1,21 @@
 console.log("Background script loaded and ready");
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === 'login') {
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError.message);
+        } else {
+          console.log('Token acquired:', token);
+          // You can now use the token to access Google APIs.
+          fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
+            .then(response => response.json())
+            .then(data => {
+              console.log(data); // This will log the user's profile information to the console.
+            })
+            .catch(error => console.error('Error:', error));
+        }
+      });
+    }
+  });
+  
