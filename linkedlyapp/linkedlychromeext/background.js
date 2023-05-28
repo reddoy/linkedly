3fd8043c4807edb55a139a979a4f0f2165d8ca4c
reddoy <rohanomalley@gmail.com>
2023-05-28 00:32:53 -0700

    uyy

--- linkedlyapp/linkedlychromeext/background.js
+++ linkedlyapp/linkedlychromeext/background.js
@@ -1,21 +1,47 @@
 console.log("Background script loaded and ready");
 
+chrome.identity.getAuthToken({interactive: false}, function(token) {
+  if (token) {
+    console.log('User is already logged in');
+    // You can now use the token to access Google APIs.
+    fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // This will log the user's profile information to the console.
+      })
+      .catch(error => console.error('Error:', error));
+  } else {
+    console.log('User is not logged in');
+    // You can show a login button or other UI to prompt the user to log in.
+  }
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === 'login') {
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        if (chrome.runtime.lastError) {
-          console.log(chrome.runtime.lastError.message);
-        } else {
-          console.log('Token acquired:', token);
-          // You can now use the token to access Google APIs.
-          fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
-            .then(response => response.json())
-            .then(data => {
-              console.log(data); // This will log the user's profile information to the console.
-            })
-            .catch(error => console.error('Error:', error));
-        }
-      });
-    }
-  });
-  
+  if (request.message === 'login') {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+      } else {
+        console.log('Token acquired:', token);
+        // You can now use the token to access Google APIs.
+        fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data); // This will log the user's profile information to the console.
+          })
+          .catch(error => console.error('Error:', error));
+      }
+    });
+  } else if (request.message === 'logout') {
+    chrome.identity.getAuthToken({interactive: false}, function(token) {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+      } else {
+        chrome.identity.removeCachedAuthToken({token: token}, function() {
+          console.log('Token revoked:', token);
+          chrome.runtime.sendMessage({message: 'loggedOut'});
+        });
+      }
+    });
+  }
+});
--- linkedlyapp/linkedlychromeext/home.js
+++ linkedlyapp/linkedlychromeext/home.js
@@ -2,4 +2,17 @@ window.onload = function () {
     document.getElementById("createAcctBtn").onclick = function () {
         chrome.runtime.sendMessage({message: 'login'});
     }
+
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === 'loggedIn') {
+        document.getElementById('logoutButton').style.display = 'block';
+    } else if (request.message === 'loggedOut') {
+        document.getElementById('logoutButton').style.display = 'none';
+    }
+    });
+
+    document.getElementById('logoutButton').addEventListener('click', function() {
+    chrome.runtime.sendMessage({message: 'logout'});
+    });
 }
